f6ac1ed3f2f70797d2a4f9296d93ef5e0410650d
scriptsvpnlt <autoscriptfinishing@gmail.com>
2024-12-27 12:59:17 +0700

    Update ubu22.sh

--- dependencies/ubu22.sh
+++ dependencies/ubu22.sh
@@ -19,4 +19,8 @@ python3.12-minimal python3.12 python3 snapd software-properties-common sosreport
 # Instalasi paket berdasarkan OS
     sudo apt update -y
     sudo apt upgrade -y
-    sudo apt install -y "${PACKAGES[@]}"
+    sudo apt-get install -y "${PACKAGES[@]}"
+sudo apt-get install -y --no-install-recommends software-properties-common
+echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
+echo iptables-persistent iptables-persistent/autosave_v6 boolean true | debconf-set-selections
+sudo apt-get install -y speedtest-cli vnstat libnss3-dev libnspr4-dev pkg-config libpam0g-dev libcap-ng-dev libcap-ng-utils libselinux1-dev libcurl4-nss-dev flex bison make libnss3-tools libevent-dev bc rsyslog dos2unix zlib1g-dev libssl-dev libsqlite3-dev sed dirmngr libxml-parser-perl build-essential gcc g++ python htop lsof tar wget curl ruby zip unzip p7zip-full python3-pip libc6 util-linux build-essential msmtp-mta ca-certificates bsd-mailx iptables iptables-persistent netfilter-persistent net-tools openssl ca-certificates gnupg gnupg2 ca-certificates lsb-release gcc shc make cmake git screen socat xz-utils apt-transport-https gnupg1 dnsutils cron bash-completion ntpdate chrony jq openvpn easy-rsa
